f8b59bf230e7132fb074fc8a71adde5823023e9a
INFO\32658368860 <wagnwnki@gmail.com>
2017-10-19 21:53:52 -0200

    feedback + MQTT

--- Feedback_MQTT/FeedbackMQTT/FeedbackMQTT.ino
+++ Feedback_MQTT/FeedbackMQTT/FeedbackMQTT.ino
@@ -1,4 +1,13 @@
+#include <SPI.h>
+#include <UIPEthernet.h>
+#include <utility/logging.h>
+#include <PubSubClient.h>
 
+
+
+//função feedback
+void feedback(char msg)
+{
 
 int ledVerde = 8;
 int ledAmarelo = 7;
@@ -6,48 +15,33 @@ int ledVermelho = 6;
 char estadoconexao = 0;
 int buzz = 5;
 
-void setup()
-{
+
   pinMode(ledVerde, OUTPUT);
   pinMode(ledAmarelo, OUTPUT);
   pinMode(ledVermelho, OUTPUT);
   pinMode(buzz, OUTPUT);
   Serial.begin(9600);
-  }
-
-void loop()
-{
-  estadoconexao = Serial.read();
-  Serial.print(estadoconexao);
   
+  estadoconexao = msg;
+   
   switch (estadoconexao){
     case 'r': {
       digitalWrite(ledVerde,LOW);
       digitalWrite(ledAmarelo,LOW);
       digitalWrite(ledVermelho,HIGH); //sistema desconectado
       delay(100);
-  	  digitalWrite(ledVermelho,LOW);
+      digitalWrite(ledVermelho,LOW);
       delay(100);
       break;
     }
     case 'y':  {
-      while (estadoconexao == 'y'){
-      
-      if(Serial.available() > 0){
-      estadoconexao = Serial.read();
-      }
-
       digitalWrite(ledVermelho,LOW);
       digitalWrite(ledVerde,LOW);
       digitalWrite(ledAmarelo,HIGH); //sistema em conexÃ£o
-      delay(100);
-  	  digitalWrite(ledAmarelo,LOW);
-      delay(100);
-  
-      }
       break;
+      }
       
-  }
+  
     case 'g':{
     digitalWrite(ledVermelho,LOW);  //sistema conectado
     digitalWrite(ledAmarelo,LOW);
@@ -100,3 +94,57 @@ void loop()
 }
 }
 
+
+// Update these with values suitable for your network.
+byte mac[] = {0xDE, 0xED, 0xBA, 0xFE, 0xF1, 0x64};
+
+// Callback function header
+void callback(char *topic, byte *payload, unsigned int length);
+
+EthernetClient ethClient;
+
+// Dados do MQTT Cloud
+PubSubClient client("m11.cloudmqtt.com", 11084, callback, ethClient);
+
+// Funcçao que irá receber o retorno do servidor.
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  char c = payload;
+  Serial.println(c);
+  Serial.println(topic);
+
+ 
+
+  byte *p = (byte *)malloc(length);
+  memcpy(p, payload, length);
+  free(p);
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Iniciando...");
+  Ethernet.begin(mac);
+  feedback('y');
+
+  // Faz a conexão no cloud com nome do dispositivo, usuário e senha respectivamente
+  if (client.connect("arduino", "arduino", "ard123"))
+  {
+    // Envia uma mensagem para o cloud no topic portao
+    client.publish("teste", "1");
+
+    // Conecta no topic para receber mensagens
+    client.subscribe("portao");
+    client.subscribe("lampada");
+    
+    Serial.println("conectado Temperatura");
+  }else{
+    Serial.println("erro ao conectar");  
+  }
+  
+}
+
+void loop()
+{
+  client.loop();
+}
