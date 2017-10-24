567b4af3f53872a04d89b7a5e3f8b02261c0ceac
INFO\32658368860 <wagnwnki@gmail.com>
2017-10-24 18:56:08 -0200

    código integrado e pronto

--- Feedback_MQTT/FeedbackMQTT/FeedbackMQTT.ino
+++ Feedback_MQTT/FeedbackMQTT/FeedbackMQTT.ino
@@ -3,7 +3,15 @@
 #include <utility/logging.h>
 #include <PubSubClient.h>
 
+#include <Servo.h>
 
+Servo portao;  
+
+int pos = 0; 
+const int pinoBotao = 4;
+const int lampada =  3;
+int estadoBotao = 0;
+int ultimoEstadoBotao;
 
 //função feedback
 void feedback(char msg)
@@ -77,13 +85,55 @@ int buzz = 5;
   }
 }
 
+void acendeLampada()
+{
+  digitalWrite(lampada, HIGH);
+  ultimoEstadoBotao = 0;
+  Serial.println("Lampada acesa...");
+  delay(215);
+}
 
+void apagaLampada()
+{
+  digitalWrite(lampada, LOW);
+  ultimoEstadoBotao = 1;
+  Serial.println("Lampada apagada...");
+  delay(215);  
+}
+
+void abrePortao()
+{
+  for (pos = 0; pos <= 89; pos += 1) 
+  { 
+    portao.write(pos);
+    delay(15);                      
+  }
+  Serial.println("Portão Aberto...");
+  ultimoEstadoBotao = 0;
+  delay(200); 
+}
+
+void fechaPortao()
+{
+  for (pos = 90; pos >= 1; pos -= 1) 
+  {
+    portao.write(pos);              
+    delay(15);                      
+  }
+  Serial.println("Portão Fechado...");
+  ultimoEstadoBotao = 1;
+  delay(200); 
+}
+
+
+char payload = 0;
+char topic = 0;
 
 // Update these with values suitable for your network.
 byte mac[] = {0xDE, 0xED, 0xBA, 0xFE, 0xF1, 0x64};
 
 // Callback function header
-void callback(char *topic, byte *payload, unsigned int length);
+void callback(char* topic, byte* payload, unsigned int length);
 
 EthernetClient ethClient;
 
@@ -91,15 +141,38 @@ EthernetClient ethClient;
 PubSubClient client("m11.cloudmqtt.com", 11084, callback, ethClient);
 
 // Funcçao que irá receber o retorno do servidor.
-void callback(char *topic, byte *payload, unsigned int length)
+void callback(char* topic, byte* payload, unsigned int length)
 {
-  char c = payload;
-  Serial.println(c);
+  char* payloadAsChar = payload;
+  payloadAsChar[length]=0;
+  String msgRecebida = String (payloadAsChar);
+  
+  Serial.println(msgRecebida);
+  Serial.println();
   Serial.println(topic);
+  delay(100);
+  feedback('d');
 
- 
+//
+ultimoEstadoBotao = 1;
+estadoBotao = digitalRead(pinoBotao);
+if(msgRecebida == "abre" && ultimoEstadoBotao == 1)
+  {
+    abrePortao();
+    acendeLampada();
+    Serial.println("");
+  }
 
-  byte *p = (byte *)malloc(length);
+  estadoBotao = digitalRead(pinoBotao);
+  
+  if(msgRecebida == "abre" && ultimoEstadoBotao == 0)  
+  {
+    fechaPortao();
+    apagaLampada();
+    Serial.println("");
+  }
+
+  byte* p = (byte* )malloc(length);
   memcpy(p, payload, length);
   free(p);
 }
@@ -115,32 +188,54 @@ void setup()
   if (client.connect("arduino", "arduino", "ard123"))
   {
     // Envia uma mensagem para o cloud no topic portao
-    client.publish("teste", "1");
-    feedback('e');
+    client.publish("teste", "v");
+    feedback('g');
+    
 
     // Conecta no topic para receber mensagens
-    if(client.subscribe("teste")==1){
-    feedback('g');
-    feedback('d');
+  client.subscribe("mensagem");
+  
+//   
+  Serial.println("Conectado MQTT");
+  delay(50);
     
-    Serial.println("Conectado MQTT");
-    
-    }
   }else{
     Serial.println("erro ao conectar"); 
     delay(50); 
     feedback('r');
   }
+
+//  setup do portão manual
+   portao.attach(9);
+  pinMode(lampada, OUTPUT);  
+  pinMode(pinoBotao, INPUT_PULLUP);
+  Serial.begin(9600);
   
+  portao.write(0);
+  digitalWrite(lampada, LOW);
+  ultimoEstadoBotao = 1;
 }
 
 void loop()
 {
   client.loop();
-  client.publish("mensagem","2");
-  if (client.subscribe("mensagem")== 2){
-      feedback('d');
-      Serial.println("Recebido");
-      }
 
+//abir e fechar portão com botão
+  estadoBotao = digitalRead(pinoBotao);
+    
+  if(estadoBotao == LOW && ultimoEstadoBotao == 1)
+  {
+    abrePortao();
+    acendeLampada();
+    Serial.println("");
+  }
+
+  estadoBotao = digitalRead(pinoBotao);
+  
+  if(estadoBotao == LOW && ultimoEstadoBotao == 0)  
+  {
+    fechaPortao();
+    apagaLampada();
+    Serial.println("");
+  }
 }
